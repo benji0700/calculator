2f90f075890728b4f745f9d1efd3c14146fe3d8a
benji0700 <benji1807@outlook.com>
2025-05-20 13:05:28 +0200

    no comments

--- calculator.py
+++ calculator.py
@@ -8,4 +8,4 @@ if __name__ == "main":
     print("5 + 3 =", add(5, 3))
     print("5 - 3 =", sub(5, 3))
 
-# This is a simple calculator module that provides basic arithmetic operations.
+# This is a simple calculator module thatj provides basic arithmetic operations.#
