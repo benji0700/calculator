f567564c2a29a75544c7c8611ca5f895c37480e8
benji0700 <benji1807@outlook.com>
2025-05-27 12:36:04 +0200

    unittests hinzugefügt

--- calculator.py
+++ calculator.py
@@ -1,11 +1,11 @@
+# calculator.py
+
 def add(a, b):
     return a + b
 
 def sub(a, b):
     return a - b
 
-if __name__ == "main":
+if __name__ == "__main__":
     print("5 + 3 =", add(5, 3))
     print("5 - 3 =", sub(5, 3))
-
-# This is a simple calculator module thatj provides basic arithmetic operations.#
